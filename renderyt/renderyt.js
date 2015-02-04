a0aabaf11c40c78aa93a40e9a5bde52f4799341d
root <root@wmaiz-v-sofa02>
2015-02-04 18:04:05 +0100

    crop

--- renderyt/renderyt.js
+++ renderyt/renderyt.js
@@ -75,7 +75,7 @@ function captureScreen(driver, imagedimensions, posttarget, onCompleted) {
 	{
 		gm(image, "temp.png")
 			.options({imageMagick: true})
-			.crop(imagedimensions.width, imagedimensions.height, imagedimensions.x, imagedimensions.y)
+			.crop(imagedimensions.width-10, imagedimensions.height, imagedimensions.x, imagedimensions.y)
 			.toBuffer('PNG',function (err, buffer) {
 				saveImage(buffer, {"width":imagedimensions.width, "height": imagedimensions.height}, posttarget, onCompleted);
 			});
